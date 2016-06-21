af3fd68c6e364f7e5535705277a4152fd46672b2
Antti Jaakkola <annttu@git.annttu.fi>
2016-06-21 09:15:22 +0300

    secure sleep

--- scripts/secure_sleep.sh
+++ scripts/secure_sleep.sh
@@ -6,3 +6,9 @@ sudo pmset -a hibernatemode 25 autopoweroff 1 autopoweroffdelay 1
 
 # Destroy file vault password from memory and hibernate image
 sudo pmset -a destroyfvkeyonstandby 1
+
+# Disable power nap
+sudo /usr/libexec/PlistBuddy -c 'Set "Custom Profile":"AC Power":DarkWakeBackgroundTasks 1' \
+    /Library/Preferences/SystemConfiguration/com.apple.PowerManagement.plist
+
+echo "Reboot require to disable power nap"
