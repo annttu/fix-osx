e05f4ed1ab9705715041fa876a31b91d5e94a738
Antti Jaakkola <annttu@git.annttu.fi>
2016-11-22 09:21:38 +0200

    fix secure sleep script

--- scripts/secure_sleep.sh
+++ scripts/secure_sleep.sh
@@ -7,8 +7,11 @@ sudo pmset -a hibernatemode 25 autopoweroff 1 autopoweroffdelay 1
 # Destroy file vault password from memory and hibernate image
 sudo pmset -a destroyfvkeyonstandby 1
 
+# Disable annoying waking features, which cause problems with hibernate
+sudo pmset -a standbydelay 0 standby 0 powernap 0
+
 # Disable power nap
-sudo /usr/libexec/PlistBuddy -c 'Set "Custom Profile":"AC Power":DarkWakeBackgroundTasks 1' \
+sudo /usr/libexec/PlistBuddy -c 'Set "Custom Profile":"AC Power":DarkWakeBackgroundTasks 0' \
     /Library/Preferences/SystemConfiguration/com.apple.PowerManagement.plist
 
 echo "Reboot require to disable power nap"
