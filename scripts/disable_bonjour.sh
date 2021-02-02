d70754d2edaf7b44c568f90ceaf23fbcb51279e7
Antti Jaakkola <antti.jaakkola@remod.fi>
2021-02-02 13:45:21 +0200

    Update bonjour script

--- scripts/disable_bonjour.sh
+++ scripts/disable_bonjour.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+
+if [ $(sw_vers -productVersion |cut -d '.' -f 2) -lt 11 ]
+then
+# For macos < 10.11
 sudo defaults write /System/Library/LaunchDaemons/com.apple.mDNSResponder ProgramArguments -array-add "-NoMulticastAdvertisements"
 
+else
+# For macos >= 10.11
+sudo defaults write /Library/Preferences/com.apple.mDNSResponder.plist NoMulticastAdvertisements -bool true
+
+fi
